a5082531c056badaffe749a9910ab824b6af8d01
Diego Leal <70763447+LealDias@users.noreply.github.com>
2024-01-30 17:43:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const textarea = document.querySelector('textarea');
+    const outputBox = document.querySelector('.output-box');
+    const resultContent = document.querySelector('.result-content');
+    const copyButton = document.querySelector('.copy-button');
+    const encryptBtn = document.querySelector('.encrypt-btn');
+    const decryptBtn = document.querySelector('.decrypt-btn');
+  
+    encryptBtn.addEventListener('click', encryptText);
+    decryptBtn.addEventListener('click', decryptText);
+  
+    function encryptText() {
+      const originalText = textarea.value;
+      if (validateInput(originalText)) {
+        const encryptedText = encrypt(originalText.toLowerCase());
+        displayResult(encryptedText);
+        toggleCopyButton(true);
+      } else {
+        showModal('Por favor, digite apenas letras minúsculas sem acentos ou caracteres especiais');
+      }
+    }
+  
+    function decryptText() {
+      const encryptedText = textarea.value;
+      if (validateInput(encryptedText)) {
+        const decryptedText = decrypt(encryptedText.toLowerCase());
+        displayResult(decryptedText);
+        toggleCopyButton(true);
+      } else {
+        showModal('Por favor, digite apenas letras minúsculas sem acentos ou caracteres especiais');
+      }
+    }
+  
+    function validateInput(text) {
+      return /^[a-z]+$/.test(text.replace(/\s/g, ''));
+    }
+  
+    function displayResult(result) {
+      const imageUrl = 'imgs/granted.png';
+      const resultHtml = `<img src="${imageUrl}" alt="Imagem de resultado" style="width: 150px;"><p>${result}</p>`;
+      resultContent.innerHTML = resultHtml;
+    }
+  
+    function toggleCopyButton(show) {
+      copyButton.style.display = show ? 'block' : 'none';
+    }
+  
+    function showModal(message) {
+      alert(message);
+    }
+  
+    function encrypt(text) {
+      const mapping = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+      };
+  
+      return text.replace(/[eioua]/g, match => mapping[match]);
+    }
+  
+    function decrypt(text) {
+      const mapping = {
+        'enter': 'e',
+        'imes': 'i',
+        'ai': 'a',
+        'ober': 'o',
+        'ufat': 'u'
+      };
+  
+      return text.replace(/(enter|imes|ai|ober|ufat)/g, match => mapping[match]);
+    }
+  
+    function copyText() {
+      const textToCopy = resultContent.querySelector('p').innerText;
+  
+      navigator.clipboard.writeText(textToCopy).then(function() {
+        alert('Texto copiado com sucesso!');
+      }).catch(function(err) {
+        console.error('Erro ao copiar texto: ', err);
+      });
+    }
+  });
